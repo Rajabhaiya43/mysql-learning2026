71ef270075fa1a1ae98e09e18a78a42d2fe7ca45
SHIVAM UPADHAYAY <itzrajaofficial143@gmail.com>
2026-06-12 13:06:28 +0530

    Update SELECT_STATEMENT_TUT.sql

--- SELECT_STATEMENT_TUT.sql
+++ SELECT_STATEMENT_TUT.sql
@@ -1,1 +1,26 @@
+SELECT*
+FROM parks_and_recreation.employee_demographics;
+
+SELECT first_name
+FROM parks_and_recreation.employee_demographics;
+
+SELECT DISTINCT gender
+FROM parks_and_recreation.employee_demographics;
+
+SELECT Age,
+Age + 10,
+(Age + 10) * 10
+FROM parks_and_recreation.employee_demographics;
+
+SELECT first_name
+FROM parks_and_recreation.employee_demographics;
+
+#PEMDAS
+#P- PARAENTESIS
+#E- EXPONENT
+#M- MULTIPLICATION
+#D- DIVISION
+#A- ADDITION
+#S- SUBSTRACTION
+
 
